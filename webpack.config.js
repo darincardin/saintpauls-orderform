e4db0c7612dd98f6716e00b19b422ede4e4a644d
darincardin <darincardin@gmail.com>
2020-02-23 08:27:39 -0500

    restructure

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,19 @@ const path = require('path');
 
 var config = {
   // 打包的入口文件
-  entry: './main.js',
+  entry: './src/main.js',
 
   // 配置打包结果，path定义输出文件夹，filename定义打包结果文件的名称
   output: {
-	path: path.resolve(__dirname),
+	path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
   },
 
   // 设置服务器端口号
   devServer: {
+	contentBase: './src',
+	publicPath: '/',
+	historyApiFallback: true,
     inline: true,
     port: 7777
   },
