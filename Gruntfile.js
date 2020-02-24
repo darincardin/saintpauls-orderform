ef4c4ec775019dea17179c26fe3c2b1625a23eef
darincardin <darincardin@gmail.com>
2020-02-24 07:33:08 -0500

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@ module.exports = function(grunt) {
 		useminPrepare: {  html: 'src/index.html',options: { dest: 'dist' }},
 		usemin:{html:['dist/index.html']  },
 		copy:{ 
+		
+		
 			html: {src: './src/index.html', dest: 'dist/index.html' },
 			images: {cwd: './src/assets/images', src: '*', dest: 'dist/assets/images',  expand: true },
 			fonts: {cwd: './src/assets/fonts', src: '*', dest: 'dist/assets/fonts',  expand: true  }
